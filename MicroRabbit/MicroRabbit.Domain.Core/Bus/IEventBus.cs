4ea8427154597b4c18b068d38cb1cd0b3c64dd01
Nishant Kumar <nkumar1@galaxe.com>
2019-11-15 22:31:17 +0530

    Initial commit: Domain object

---
+++ MicroRabbit/MicroRabbit.Domain.Core/Bus/IEventBus.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using MicroRabbit.Domain.Core.Commands;
+using MicroRabbit.Domain.Core.Events;
+
+namespace MicroRabbit.Domain.Core.Bus
+{
+    public interface IEventBus
+    {
+        Task SendCommand<T>(T command) where T : Command;
+        void PublishEvent<T>(T @event) where T : Event; //to use c# reserved keyword use @ prior to it, event is c# reserve ketword.
+        void Subscribe<T, TH>()
+            where T : Event
+            where TH : IEventHandler<T>;
+    }
+}
